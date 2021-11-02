56793441081706698acf6a0c809284147ee86840
Yangsuhyuck <pplloo748@gmail.com>
2021-11-02 20:11:22 +0900

    패스트 캠퍼스 Basic-Ch07-녹음기

--- app/src/main/java/com/example/basic_ch07_audiorecorder/MainActivity.kt
+++ app/src/main/java/com/example/basic_ch07_audiorecorder/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.basic_ch07_audiorecorder
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.basic_ch07_audiorecorder.databinding.ActivityMainBinding
@@ -9,26 +13,168 @@ class MainActivity : AppCompatActivity() {
     lateinit var mainBinding : ActivityMainBinding
 
     private var state = State.BEFORE_RECODING   // 초기 상태는 녹음 전
+    set(value) {
+        field = value
+        mainBinding.resetBtn.isEnabled = (value == State.AFTER_RECODING) || (value == State.ON_PLAYING)
+        mainBinding.recordBtn.updateIconWithState(value)
+    }
+
+    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO , Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    private var recorder:MediaRecorder? = null
+    private var player:MediaPlayer? = null
+
+    private val recordingFilePath: String by lazy {
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
+        requestAudioPermission()
         initViews()
+        bindViews()
+        initVariables()
 
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted =
+            (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) && (grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED)
+
+        if (!audioRecordPermissionGranted) {
+            finish()    // 앱 종료
+        }
+
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions , REQUEST_RECORD_AUDIO_PERMISSION)
     }
 
     private fun initViews() {
         mainBinding.recordBtn.updateIconWithState(state)
     }
 
+    private fun bindViews() {
+
+        mainBinding.SoundVisualizerView.onRequestCurrentAmplitude = {
+            recorder?.maxAmplitude ?: 0
+        }
+
+        mainBinding.recordBtn.setOnClickListener {
+            when(state) {
+                State.BEFORE_RECODING -> {
+                    startRecording()
+                }
+
+                State.ON_RECODING -> {
+                    stopRecording()
+                }
+
+                State.AFTER_RECODING -> {
+                    startPlaying()
+                }
+
+                State.ON_PLAYING -> {
+                    stopPlaying()
+                }
+            }
+        }
+
+        mainBinding.resetBtn.setOnClickListener {
+            stopPlaying()
+            mainBinding.SoundVisualizerView.clearVisualization()
+            mainBinding.recordTimeTextView.clearCountUp()
+            state = State.BEFORE_RECODING
+        }
 
 
+    }
+
+    private fun initVariables() {
+        state = State.BEFORE_RECODING
+    }
 
 
+    private fun startPlaying() {
+        player = MediaPlayer().apply {
+            setDataSource(recordingFilePath)
+            prepare()
+        }
+
+        // 전달된 파일을 모두 재생했을 경우 실행
+        player?.setOnCompletionListener {
+            stopPlaying()
+            state = State.AFTER_RECODING
+        }
+
+        player?.start()
+        mainBinding.SoundVisualizerView.startVisualizing(true)
+        mainBinding.recordTimeTextView.startCountUp()
+
+        state = State.ON_PLAYING
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+        mainBinding.SoundVisualizerView.stopVisualizing()
+        mainBinding.recordTimeTextView.stopCountUp()
+
+        state = State.AFTER_RECODING
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(recordingFilePath)
+            prepare()
+        }
+        recorder?.start()
+        mainBinding.SoundVisualizerView.startVisualizing(false)
+        mainBinding.recordTimeTextView.startCountUp()
+
+        state = State.ON_RECODING
+    }
+
+    private fun stopRecording() {
+        recorder?.run {
+            stop()
+            release()
+        }
+
+        recorder = null
+        mainBinding.SoundVisualizerView.stopVisualizing()
+        mainBinding.recordTimeTextView.stopCountUp()
+
+        state = State.AFTER_RECODING
+    }
+
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
+    }
 
 
 
 
 }
+
+/*
+companion object?
+costomSetter?
+invoke?
+
+
+ */
---
+++ app/src/main/java/com/example/basic_ch07_audiorecorder/SoundVisualizerView.kt
@@ -0,0 +1,112 @@
+package com.example.basic_ch07_audiorecorder
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class SoundVisualizerView(context: Context , attrs: AttributeSet? = null) : View(context,attrs) {
+
+    var onRequestCurrentAmplitude: (() -> Int)? = null
+
+    private val amplitudePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {    // 계단화 방지 플래그
+        color = context.getColor(R.color.purple_500)
+        strokeWidth = LINE_WIDTH
+        strokeCap = Paint.Cap.ROUND     // 라인의 양 끝을 어떻게 처리할 것인가?
+    }
+
+    private var drawingWidth:Int = 0
+    private var drawingHeight:Int = 0
+    private var drawingAmplitudes: List<Int> = emptyList()
+
+    private var isReplaying = false
+    private var replayingPosition: Int = 0
+
+    private val visualizeRepeatAction : Runnable = object : Runnable {
+        override fun run() {
+
+            // Amplitude 가져오기 , Draw
+            if (!isReplaying) {
+                val currentAmplitude = onRequestCurrentAmplitude?.invoke() ?: 0
+                drawingAmplitudes = listOf(currentAmplitude) + drawingAmplitudes    // 제일 최근 진폭이 제일 앞에 있어야 한다
+            } else {
+                replayingPosition++
+            }
+
+            invalidate()    // 뷰 갱신을 위해 선언 -> 다시 Drawing
+
+            handler?.postDelayed(this, ACTION_INTERVAL)    // 자기 자신을 20ms뒤에 다시 실행
+
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        drawingHeight = h
+        drawingWidth = w
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        canvas ?: return    // canvas가 null일 경우 진행 X
+
+        val centerY = drawingHeight / 2f
+        var offsetX = drawingWidth.toFloat()
+
+        drawingAmplitudes.let { amplitudes ->
+            if (isReplaying) {
+                amplitudes.takeLast(replayingPosition)
+            } else {
+                amplitudes
+            }
+        }
+            .forEach { amplitude ->
+            val lineLength = amplitude / MAX_AMPLITUDE * drawingHeight * 0.8F
+
+            offsetX -= LINE_SPACE
+            if (offsetX < 0) {
+                return@forEach
+            }
+
+            canvas.drawLine(
+                offsetX,
+                centerY - lineLength / 2F,
+                offsetX,
+                centerY + lineLength / 2F,
+                amplitudePaint
+            )
+
+
+        }
+
+    }
+
+    fun startVisualizing(isReplaying:Boolean) {
+
+        this.isReplaying = isReplaying
+        handler?.post(visualizeRepeatAction)
+
+    }
+
+    fun stopVisualizing() {
+
+        replayingPosition = 0
+        handler?.removeCallbacks(visualizeRepeatAction)
+
+    }
+
+    fun clearVisualization() {
+        drawingAmplitudes = emptyList()
+        invalidate()
+    }
+
+    companion object {
+        private const val LINE_WIDTH = 10F
+        private const val LINE_SPACE = 15F
+        private const val MAX_AMPLITUDE = Short.MAX_VALUE.toFloat()   // 진폭의 최대값 = short 값의 최대값으로 제한을 둔다 32767
+        private const val ACTION_INTERVAL = 20L
+    }
+
+}
